b1b827db666d54d9396ce7f32e317d03f922a484
GTM-Winter <dimastriana03@gmail.com>
2022-04-19 14:13:02 +0700

    Apply Component TopBar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 import TopBar from "./component/TopBar";
 function App() {
-  return <div className="App">{TopBar}</div>;
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
 }
 
 export default App;
